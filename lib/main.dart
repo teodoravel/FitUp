e2cc751e5b636f8c3bb57adbfb199c12554b1bcc
teodoravel <veljanoskateodora9@gmail.com>
2025-01-21 14:41:14 -0500

    Revert "Add feature: Map widget, updated screens, etc."
    This reverts commit 18fbff871761279667e63c18833f8c2816e6a6dd.
    
    wq:
    
    wq:
    commit change

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
-// main.dart
+<<<<<<< HEAD
 import 'package:flutter/material.dart';
 
-// Intro & Start
-import 'pages/introduction_page.dart';
-import 'pages/start_page.dart';
+// Introduction & Start
+import 'pages/introduction_page.dart';    // class Introduction
+import 'pages/start_page.dart';          // class StartPage
 
 // Auth
 import 'pages/login_page.dart';
@@ -12,6 +12,18 @@ import 'pages/register_page2.dart';
 import 'pages/success_registration_page.dart';
 
 // Home / Profile / Account
+=======
+import 'package:fitup/pages/gyms/gym_list_page.dart';
+import 'package:fitup/pages/register_page1.dart';
+import 'package:fitup/pages/inroduction_page.dart';
+import 'package:fitup/pages/register_page2.dart';
+import 'package:fitup/pages/start_page.dart';
+import 'package:fitup/pages/success_registration_page.dart';
+import 'package:fitup/pages/workout_details_page.dart';
+import 'package:fitup/pages/workout_tracker/workout_tracker_outdoor.dart';
+import 'package:flutter/material.dart';
+import 'pages/login_page.dart';
+>>>>>>> 749e1343f1c35b30465549d45fe685bc124b4d5b
 import 'pages/home_page.dart';
 import 'pages/profile_page.dart';
 import 'pages/account_page.dart';
@@ -27,13 +39,14 @@ import 'pages/gyms/gym_map_page.dart';
 import 'pages/gyms/trainer_details_page.dart';
 
 // Workout Details
-import 'pages/workout_details/workout_details_page.dart';
-import 'pages/workout_details/workout_details2.dart';
-import 'pages/workout_details/start_workout.dart';
+import 'pages/workout_details/workout_details_page.dart';  // class WorkoutDetailsPage
+import 'pages/workout_details/workout_details2.dart';      // class WorkoutDetails2Page
+import 'pages/workout_details/start_workout.dart';         // class StartWorkoutPage
 
 // Workout Schedule
-import 'pages/workout_schedule.dart';
-import 'pages/add_workout_schedule.dart';
+// (NOTE: these two are NOT in the workout_tracker subfolder, but at the same pages/ level)
+import 'pages/workout_schedule.dart';           // class WorkoutSchedulePage
+import 'pages/add_workout_schedule.dart';       // class AddWorkoutSchedulePage
 
 void main() {
   runApp(const FitUpApp());
@@ -47,13 +60,19 @@ class FitUpApp extends StatelessWidget {
     return MaterialApp(
       title: 'FitUp',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.purple),
-
-      // Choose your initial screen
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+<<<<<<< HEAD
+      // Start with the introduction page
       home: const Introduction(),
 
+=======
+      // whichever initial screen you prefer
+      home: const WorkoutDetailsPage(),
+>>>>>>> 749e1343f1c35b30465549d45fe685bc124b4d5b
       routes: {
-        // Basic
+        // Intro & Start
         '/introduction': (context) => const Introduction(),
         '/start': (context) => const StartPage(),
 
@@ -72,13 +91,14 @@ class FitUpApp extends StatelessWidget {
         '/workoutIndoor': (context) => const WorkoutTrackerIndoorPage(),
         '/workoutOutdoor': (context) => const WorkoutTrackerOutdoorPage(),
         '/workoutFavorite': (context) => const WorkoutTrackerFavoritePage(),
+        '/workoutDetails': (context) => const WorkoutDetailsPage(),
 
         // Gyms
         '/gyms': (context) => const GymListPage(),
         '/gymMap': (context) => const GymMapPage(),
         '/trainerDetails': (context) => const TrainerDetailsPage(),
 
-        // Workout details
+        // Workout Details
         '/workoutDetails': (context) => const WorkoutDetailsPage(),
         '/workoutDetails2': (context) => const WorkoutDetails2Page(),
         '/startWorkout': (context) => const StartWorkoutPage(),
